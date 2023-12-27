830387c696fbe6798ecc3e9f35e674197bd78a59
EshuDevs <yamindevs@gmail.com>
2023-12-27 21:02:38 +0100

    made the rock-paper-scissors game logic

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -1,10 +1,28 @@
-const userRock = document.querySelector('.user-rock');
-const userPaper = document.querySelector('.user-paper')
-const userScissor = document.querySelector('.user-scissor')
+const rock = document.querySelector('.user-rock');
+const paper = document.querySelector('.user-paper')
+const scissor = document.querySelector('.user-scissor')
 
 const options = ['rock', 'paper', 'scissors']
 
-const getComputerChoice = () => {
-    return options[Math.floor(Math.random() * options.length)]
-};
+
+const game = (choice) => {
+    let computerChoice = () => {
+        return options[Math.floor(Math.random() * options.length)]
+    };
+
+    computerChoice = computerChoice()
+
+    if(choice === computerChoice){
+        console.log('it\'s a tie')
+    }
+    else if(
+    choice === 'rock' && computerChoice === 'scissors' ||
+    choice === 'paper' && computerChoice === 'rock' ||
+    choice === 'scissors' && computerChoice === 'paper'){
+        console.log('you win')
+    }
+    else{
+        console.log('you lose')
+    }
+}
 
