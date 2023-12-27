90627b4f82190e9d30265a47dd5ea06e13aaed8d
EshuDevs <yamindevs@gmail.com>
2023-12-27 20:17:35 +0100

    first commit

---
+++ scripts/rock-paper-scissors.js
@@ -0,0 +1,9 @@
+const userRock = document.querySelector('.user-rock');
+const userPaper = document.querySelector('.user-paper')
+const userScissor = document.querySelector('.user-scissor')
+const comRock = document.querySelector('.com-rock')
+const comPaper = document.querySelector('.com-paper')
+const comScissor = document.querySelector('.com-scissor')
+
+
+
